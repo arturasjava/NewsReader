6baa5cc8ed0e066f25857b7d55555e73da1bbad4
Arturas <arturas.mikaila@gmail.com>
2022-02-02 12:55:06 +0200

    Setup adapter and configure list view template

--- app/src/main/java/lt/vcs/newsreader/MainActivity.java
+++ app/src/main/java/lt/vcs/newsreader/MainActivity.java
@@ -3,12 +3,25 @@ package lt.vcs.newsreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+     //https://github.com/HackerNews/API
+
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
