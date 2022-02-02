2447279d2fd80c975eded1a48886ab23616d75cb
Arturas <arturas.mikaila@gmail.com>
2022-02-02 18:04:52 +0200

    Add url connection and input stream

--- app/src/main/java/lt/vcs/newsreader/MainActivity.java
+++ app/src/main/java/lt/vcs/newsreader/MainActivity.java
@@ -1,16 +1,20 @@
 package lt.vcs.newsreader;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-     //https://github.com/HackerNews/API
+    //https://github.com/HackerNews/API
 
     ArrayList<String> titles = new ArrayList<>();
     ArrayAdapter arrayAdapter;
@@ -29,7 +33,22 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             return null;
         }
     }
